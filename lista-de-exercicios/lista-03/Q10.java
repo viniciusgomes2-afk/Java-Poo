c284a075d4f86cc9c393601c971a10eb8af07c80
Vinícius Gomes Alves <viniciusgomesalves25@gmail.com>
2026-08-25 21:33:34 -0300

    Fix: Bug no commit

---
+++ lista-de-exercicios/lista-03/Q10.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q10{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int ddd = sc.nextInt();
+        
+        String cidadePertencente = switch(ddd){
+            case 61 -> "Brasilia";
+            case 71 -> "Salvador";
+            case 11 -> "São Paulo";
+            case 21 -> "Rio de Janeiro";
+            case 32 -> "Juiz de Fora";
+            case 19 -> "Campinas";
+            case 27 -> "Vitoria";
+            case 31 -> "Belo Horizonte";
+            default -> "DDD nao cadastrado";
+        };
+
+        System.out.println(cidadePertencente);
+    }
+}
