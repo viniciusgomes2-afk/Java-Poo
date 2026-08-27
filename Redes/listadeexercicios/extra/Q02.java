d256f80d9fb7d9bef0f36c5c9808c4d4659b0c82
Vinícius Gomes Alves <vinicius.gomes2@escolar.ifrn.edu.br>
2026-08-27 11:51:33 -0300

    aumentando repo de questões

---
+++ Redes/listadeexercicios/extra/Q02.java
@@ -0,0 +1,18 @@
+package Redes.listadeexercicios.extra;
+
+import java.util.Scanner;
+
+public class Q02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
+        
+        int T1 = sc.nextInt();
+        int T2 = sc.nextInt();
+        int T3 = sc.nextInt();
+        int T4 = sc.nextInt();
+
+        System.out.println((T1 + T2 + T3 + T4) - 3);
+
+        sc.close();
+    }
+}
